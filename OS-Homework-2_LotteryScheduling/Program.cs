9bb3f0db0241b742d451ddf6e0e019634077da6a
billydanke <johnjo490@gmail.com>
2024-03-07 15:08:38 -0600

    Add project files.

---
+++ OS-Homework-2_LotteryScheduling/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OS_Homework_2_LotteryScheduling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
